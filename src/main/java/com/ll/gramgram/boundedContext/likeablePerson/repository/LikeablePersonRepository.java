47c9d288436e8b4299690ecef0dfb932f7648317
박진용 <70837543+CatJelly@users.noreply.github.com>
2023-05-01 15:20:23 +0900

    2 week (#5)
    * feat: case 4 (test, duplex processing)
    
    * feat: case 5 (test, add limit)
    
    * feat: case 6(test, likeable modify)
    
    * 2Week records update
    
    * feat: refactoring issues
    
    * 2Week refactoring
    
    * 2Week Fix

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/repository/LikeablePersonRepository.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/repository/LikeablePersonRepository.java
@@ -4,16 +4,15 @@ import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface LikeablePersonRepository extends JpaRepository<LikeablePerson, Long> {
+public interface LikeablePersonRepository extends JpaRepository<LikeablePerson, Long>, LikeablePersonRepositoryCustom {
     List<LikeablePerson> findByFromInstaMemberId(Long fromInstaMemberId);
 
-    // ToInstaMember들 중에서 username인 것들을 List로 리턴
     List<LikeablePerson> findByToInstaMember_username(String username);
 
-    // FromInstaMemberId와 ToInstaMember가 username인 LikeablePerson 리턴
     LikeablePerson findByFromInstaMemberIdAndToInstaMember_username(long fromInstaMemberId, String username);
 
-
-
+    Optional<LikeablePerson> findByFromInstaMember_usernameAndToInstaMember_username(String fromInstaMemberUsername, String toInstaMemberUsername);
 }
+
