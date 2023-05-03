28b1f67f86ccb2094f9719302c8598a3c5764b86
ParkJinYong <mej1406@gmail.com>
2023-05-03 15:30:01 +0900

    feat: notify add

--- src/main/java/com/ll/gramgram/boundedContext/notification/controller/NotificationController.java
+++ src/main/java/com/ll/gramgram/boundedContext/notification/controller/NotificationController.java
@@ -10,7 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/usr/notification")
@@ -28,8 +31,12 @@ public class NotificationController {
 
         // 자기가 보낸 호감, 자기가 받은 호감에 대한 알림을 모두 찾기 위함
         List<Notification> notifications = notificationService.findByToInstaMember(rq.getMember().getInstaMember());
-
         model.addAttribute("notifications", notifications);
+
+        for(Notification notification : notifications) {
+            notification.setReadDate(LocalDateTime.now());
+            notificationService.update(notification);
+        }
 
         return "usr/notification/list";
     }
